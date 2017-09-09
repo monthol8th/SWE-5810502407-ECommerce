db7a6ae818f297511194185c4fe17b60902317f9
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-09 23:56:40 +0700

    add location to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class profile(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField(default='default description text')
+    location = models.CharField(max_length=120, default='default location')
 
     def __str__(self):
         return self.name
