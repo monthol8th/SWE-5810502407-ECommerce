5f490fa62fc9666d46b8d9ad4d7b5fd46a051b73
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-09 23:39:26 +0700

    add profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from __future__ import unicode_literals
 # Create your models here.
+
+class profile(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __unicode__(self):
+        return self.name
