5e07f93f3b9d88cfeb4ad988ec7aef5101c79842
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-10 17:42:31 +0700

    create contact view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contact(request):
+    context = locals()
+    templates = 'contact.html'
+    return render(request,templates,context)
